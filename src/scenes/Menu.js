c72af4710ed2315a9581ab4e42dd0dacca0fec75
benurlik <baurlik@ucsc.edu>
2021-05-10 14:49:20 -0700

    adding scenes

---
+++ src/scenes/Menu.js
@@ -0,0 +1,15 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    create() {
+        // this.keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    }
+
+    update() {
+        // if (Phaser.Input.Keyboard.JustDown(this.keyUP)) {
+        //     this.scene.start('playScene');    
+        // }
+    }
+}
