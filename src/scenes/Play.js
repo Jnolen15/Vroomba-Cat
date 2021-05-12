2c8104730aed9d66a2a9b930bd22f2d3661d0b30
Nathann Latimore <nlatimor@ucsc.edu>
2021-05-12 11:56:00 -0700

    added camera follower

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -41,9 +41,16 @@ class Play extends Phaser.Scene {
         this.platform = this.platformGroup.create(300, 540, "platform");
         this.platform.scaleX = 2;
         this.platform.setImmovable(true);
+
+        // Camera Follower Setup
+        this.cameras.main.setBounds(0, 0, game.config.width, game.config.height);
+        this.cameras.main.setZoom(1.3);
+        this.camFollowX = this.cameras.main.scrollX;
+        this.camFollowY = this.cameras.main.scrollY;
+        this.camFollowRate = .0045;
     }
 
-    update() {
+    update(time, delta) {
         //Update timer text
         var remaining_time = (this.game.settings.gameTimer/1000) - Phaser.Math.RoundTo(this.clock.elapsed/1000,2, 1);
         this.timeElapsed.setText('Time: ' + remaining_time);
@@ -56,5 +63,15 @@ class Play extends Phaser.Scene {
 
         // Update Player Cat
         if(!gameOver) this.cat.update();
+
+        // Update Camera Follower
+        this.camFollowPlayer(delta);
+    }
+
+    camFollowPlayer(delta) {
+        // Make camera 'catch up' to player gradually with linear interpolation
+        this.camFollowX = Phaser.Math.Linear(this.camFollowX, (this.cat.x)- game.config.width/2, this.camFollowRate * delta);
+        this.camFollowY = Phaser.Math.Linear(this.camFollowY, (this.cat.y)- game.config.height/2, this.camFollowRate * delta);
+        this.cameras.main.setScroll(this.camFollowX, this.camFollowY);
     }
 }
