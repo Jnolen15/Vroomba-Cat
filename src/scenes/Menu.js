7dbebefbe3d695b00a5a645ac060608dd4799819
benurlik <baurlik@ucsc.edu>
2021-05-17 23:02:59 -0700

    Added buttons to the main menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -21,9 +21,27 @@ class Menu extends Phaser.Scene {
         this.keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 
         // Display menu text
-        this.add.text(game.config.width/2, game.config.height/2, "Menu", textConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/1.75, "Press ↑ to go to Play Scene", textConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, "Vroomba Cat", textConfig).setOrigin(0.5);
+        // this.add.text(game.config.width/2, game.config.height/1.75, "Press ↑ to go to Play Scene", textConfig).setOrigin(0.5);
+        let startButton = this.add.text(game.config.width/2, game.config.height/1.75, "Start Game", textConfig).setOrigin(0.5);
+        let tutorialButton = this.add.text(game.config.width/2, game.config.height/1.55, "Tutorial", textConfig).setOrigin(0.5);
+        
+        // Adding buttons to the main menu
+        startButton.setInteractive();
+        tutorialButton.setInteractive();
 
+        // Interaction controls for both buttons
+        startButton.on("pointerover", () => { console.log("start hovering"); })
+        startButton.on("pointerout", () => { console.log("start not hovering"); })
+        startButton.on("pointerup", () => {
+            this.scene.start('playScene');  
+            game.settings = { 
+                gameTimer: 60000 
+            }
+        })
+        tutorialButton.on("pointerover", () => { console.log("tutorial hovering"); })
+        tutorialButton.on("pointerout", () => { console.log("tutorial not hovering"); })
+        tutorialButton.on("pointerup", () => { console.log("tutorial click") })
     }
 
     update() {
