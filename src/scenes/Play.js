505ea967b8fef21c382cc83962aaaac431fc36e4
Jared Nolen <Legodoghead@gmail.com>
2021-05-19 11:30:35 -0700

    Added Tileset Loading

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,8 +3,27 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload(){        
+        // Load Json file
+        this.load.tilemapTiledJSON('level', './assets/tilemap/tilemap_Test.json');
+        // Load Spritesheet
+        this.load.image('fridge', './assets/static_fridge.png');
+    }
+    
     create() {
+        // Add controller
         this.controller = new Controller(this);
+
+        //Create the tilemap
+        const map = this.add.tilemap('level');
+
+        // add a tileset to the map
+        const tileset = map.addTilesetImage('static_fridge', 'fridge');
+
+        // create tilemap layers
+        const FurnitureLayer = map.createLayer('FridgeLayer', tileset);
+        
+        /*this.controller = new Controller(this);
         
         // spawn and place objects
         this.controller.spawner.createProp('prop', game.config.width*0.25, game.config.height*0.7, 0.5);
@@ -15,10 +34,10 @@ class Play extends Phaser.Scene {
         this.controller.spawner.createPlatform('platform', game.config.width/2, game.config.height, 8, 1);
         this.controller.spawner.createPlatform('platform', 700, 400, 4, 1);
         this.controller.spawner.createPlatform('platform', 100, 300, 3, 1);
-        this.controller.spawner.createPlatform('platform', 300, 540, 2, 1);
+        this.controller.spawner.createPlatform('platform', 300, 540, 2, 1);*/
     }
 
     update(time, delta) {
-        this.controller.update(time, delta);
+        //this.controller.update(time, delta);
     }
 }
