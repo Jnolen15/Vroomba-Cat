f2b818a294c24eb3871960dfe12083a241ae97f7
benurlik <baurlik@ucsc.edu>
2021-05-26 19:10:45 -0700

    added a different title animation

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,13 +11,12 @@ class Menu extends Phaser.Scene {
         this.keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
         // Display menu text
-        this.title = this.add.text(game.config.width/2, 0, "Vroomba Cat", textConfig).setOrigin(0.5);
+        this.title = this.add.text(game.config.width/2, game.config.height * .10, "Vroomba Cat", textConfig).setOrigin(0.5);
         this.titleMoveSpeed = 5; this.stop = false; this.title.setDepth(1);
-        let startButton = this.add.text(game.config.width/2, game.config.height/1.75, "[↑] Start Game", textConfig).setOrigin(0.5);
-        let tutorialButton = this.add.text(game.config.width/2, game.config.height/1.40, "[↓] Tutorial", textConfig).setOrigin(0.5);
-        let quitButton = this.add.text(game.config.width/2 ,game.config.height/1.28, "Quit", textConfig).setOrigin(0.5);
-        this.modeIndicator = this.add.text(game.config.width/2, game.config.height/1.55, "[→] Regular Mode", textConfig).setOrigin(0.5);
-
+        let startButton = this.add.text(game.config.width/2, game.config.height * .42, "[↑] Start Game", textConfig).setOrigin(0.5);
+        let tutorialButton = this.add.text(game.config.width/2, game.config.height * .58, "[↓] Tutorial", textConfig).setOrigin(0.5);
+        let quitButton = this.add.text(game.config.width/2 ,game.config.height* .65, "Quit", textConfig).setOrigin(0.5);
+        this.modeIndicator = this.add.text(game.config.width/2, game.config.height * .5, "[→] Regular Mode", textConfig).setOrigin(0.5);
         
         // Adding buttons to the main menu
         startButton.setInteractive();
@@ -44,11 +43,21 @@ class Menu extends Phaser.Scene {
             startButton.alpha = 0;
             tutorialButton.alpha = 0;
             quitButton.alpha = 0;
+            this.modeIndicator.alpha = 0;
             this.troll = this.add.text(game.config.width/2,game.config.height/2, 
                 "HA YOU THOUGHT! YOU CAN NEVER QUIT THIS GAME BECAUSE IT IS THE BEST GAME IN THE WORLD!!!" + 
                 "REFRESH THIS PAGE NOW AND YOU BETTER ENJOY VROOMBA CAT FOR THE REST OF YOUR LIFE BECAUSE IT IS THE ONLY GAME YOU SHOULD PLAY FROM NOW ON. I WILL ACCEPT NO LESS." +
                 "VROOMBA CAT IS THE LIGHT AND IT IS THE SAVIOR. NOW GET BACK TO PLAYING THE GAME CHUMP!", textConfig).setOrigin(0.5);
         })
+        this.tweens.add({
+            targets: this.title,
+            y: game.config.height * .35,
+            ease: 'Bounce',
+            yoyo: 1,
+            repeat: -1,
+            hold: 3000,
+            duration: 1000,
+        });
     }
 
     update() {
@@ -72,18 +81,6 @@ class Menu extends Phaser.Scene {
         if(Phaser.Input.Keyboard.JustDown(this.keyDOWN)) {
             game.settings = { gameTimer: 600000}
             this.scene.start('tutorialScene');
-        }
-
-        if(this.title.y == game.config.height/2) {
-            this.titleMoveSpeed = 0;
-        }
-        else if(!this.stop && this.title.y < game.config.height *0.55) {
-            this.title.y += this.titleMoveSpeed;
-        }
-        else if(this.title.y > game.config.height/2) {
-            this.titleMoveSpeed = 1;
-            this.stop = true;
-            this.title.y -= this.titleMoveSpeed;
         }
     }
 }
